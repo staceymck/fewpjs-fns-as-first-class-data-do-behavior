9fb76a0a74fbf88f01375e06d877de80da646e95
Stacey McKnight <mcknight.stm@gmail.com>
2021-03-15 13:48:46 -0500

    commit tree 835fd04a1849f36ca6c1495efcd17f7ce81b049c

--- index.js
+++ index.js
@@ -9,5 +9,19 @@ function handleClick(e) {
 
 /* End Given Code, don't edit above here...*/
 
-/* Write your implementation of greet() */
-/* Write your implementation of displayMessage() */
+function greet(input) {
+  const time = input.split(':');
+  const hour = parseInt(time[0]);
+
+  if (hour < 12 ) {
+    return "Good Morning";
+  } else if (hour < 17) {
+    return "Good Afternoon";
+  } else {
+    return "Good Evening";
+  }
+}
+
+function displayMessage(message) {
+  document.getElementById('greeting').innerText = message;
+}
